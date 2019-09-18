b2260c8b8dc9b5811d824ce5ab49cac10f0d4bb6
Arnav Gupta <arnav@codingblocks.com>
2019-09-19 00:34:35 +0530

    basic socket server
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+  console.log('connected with socket id =', socket.id)
+
+})
+
+app.use('/', express.static(__dirname + '/public'))
+
+server.listen(3344, () => {
+  console.log('Started on http://localhost:3344')
+})
